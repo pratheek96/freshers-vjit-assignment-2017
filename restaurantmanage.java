9c908a4122d8f2b86518ab97b95bd232c53819b0
pratheek96 <32196333+pratheek96@users.noreply.github.com>
2017-09-24 22:16:56 +0530

    Add files via upload

---
+++ restaurantmanage.java
@@ -0,0 +1,55 @@
+import java.util.*;
+
+class Fun
+
+ {
+   
+   void greetings()
+
+    {
+ 
+        System.out.println("hello");
+
+    }
+   
+   void allotment(int vacancy)
+   
+    {
+ 
+       if(vacancy==0)
+ 
+       System.out.println("waiting");
+ 
+       else
+ 
+       System.out.println("table alloted");
+ 
+     }
+ 
+   
+}
+
+public class MyClass 
+ {
+ 
+   public static void main(String args[])
+
+    {
+ 
+       Fun entry=new Fun();
+
+        Fun seat=new Fun();
+
+        entry.greetings();
+
+        Scanner sc=new Scanner(System.in);
+
+        System.out.println("Enter a number of vacant seats : ");
+
+        int vacancy=sc.nextInt();
+
+        seat.allotment(vacancy);
+
+     }
+
+}
